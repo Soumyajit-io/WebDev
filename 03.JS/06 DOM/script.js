1395f3e7a81118476a40f91042f3593dd04e4482
Soumyajit-io <soumyajit010805@gmail.com>
2025-05-02 22:21:27 +0530

    code added

---
+++ 03.JS/06 DOM/script.js
@@ -0,0 +1,9 @@
+console.log("hello");
+//with id
+let a = document.getElementById("hh1");// returns the h1 value
+console.dir(a);
+// with class
+let b = document.getElementsByClassName("h1");
+console.dir(b);
+let bb = document.getElementById("btn");
+console.log(bb);
