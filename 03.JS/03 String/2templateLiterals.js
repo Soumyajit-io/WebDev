731c5972037ebd7f9012bfd0e300625058966fe2
Soumyajit-io <soumyajit010805@gmail.com>
2025-05-02 11:47:51 +0530

    Code Added

---
+++ 03.JS/03 String/2templateLiterals.js
@@ -0,0 +1,6 @@
+let obj = {
+    iteams : "pen",
+    price : 50
+};
+let str = `hello this is SoumyajiT ${obj.price}  ${4+4}`;// string interpolation
+console.log(str);
---
+++ 03.JS/03 String/3StringMethod.js
@@ -0,0 +1,4 @@
+let str = "     badBoy   Soumo  ";
+console.log(str.toUpperCase());//upper
+console.log(str.toLowerCase());//lower
+console.log(str.trim());// remove white spaces
